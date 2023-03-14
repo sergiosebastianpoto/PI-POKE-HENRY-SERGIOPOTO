1551f35098cb48f96b9ec6007eb56239da65408f
sergiosebastianpoto <sergiosebastianpoto@gmail.com>
2023-03-14 16:38:21 -0300

    agregado de columnas

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -23,6 +23,23 @@ export default function Home() {
         "https://images.wikidexcdn.net/mwuploads/wikidex/4/43/latest/20190406170624/Bulbasaur.png",
       type: "grass",
     },
+    {
+      name: "Charmander",
+      image:
+        "https://i.pinimg.com/564x/f2/95/76/f295769d9bd3c34ffc552e837f5304ae.jpg",
+      type: "fire",
+    },
+    {
+      name: "Squirtle",
+      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+      type: "water",
+    },
+    {
+      name: "Bulbasaur",
+      image:
+        "https://images.wikidexcdn.net/mwuploads/wikidex/4/43/latest/20190406170624/Bulbasaur.png",
+      type: "grass",
+    },
   ];
 
   return (
